52a26370aca90d1f3893f298064926a1960f7375
Will J <138139664+WillJTools@users.noreply.github.com>
2023-12-08 22:20:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,41 +1,39 @@
 function searchGlobals() {
-    const fileInput = document.getElementById('fileInput');
-    const file = fileInput.files[0];
-  
-    if (file) {
-      const reader = new FileReader();
-  
-      reader.onload = function(e) {
-        const content = e.target.result;
-        const globalVars = findGlobalVariables(content);
-        displayResults(globalVars);
-      };
-  
-      reader.readAsText(file);
-    } else {
-      alert('Please select a file.');
-    }
+  const fileInput = document.getElementById('fileInput');
+  const file = fileInput.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+      const content = e.target.result;
+      const globalVars = findGlobalVariables(content);
+      displayResults(globalVars);
+    };
+
+    reader.readAsText(file);
+  } else {
+    alert('Please select a file.');
   }
+}
+
+function findGlobalVariables(code) {
+  const globalVarRegex = /\b(var|let|const)\s+(\w+)\b/g;
+  const matches = code.match(globalVarRegex);
   
-  function findGlobalVariables(code) {
-    // Implement logic to search for global variables in the code
-    // This might involve using regular expressions or AST parsing
-    // Example:
-    const globalVarRegex = /(?:var|let|const)\s+(\w+)\s*=/g;
-    const matches = code.match(globalVarRegex);
-    if (matches) {
-      return matches.map(match => match.replace(/(var|let|const)\s+/, ''));
-    } else {
-      return [];
-    }
+  if (matches) {
+    const uniqueVars = [...new Set(matches.map(match => match.split(/\s+/)[1]))];
+    return uniqueVars;
+  } else {
+    return [];
   }
-  
-  function displayResults(variables) {
-    const resultContainer = document.getElementById('result');
-    if (variables.length > 0) {
-      resultContainer.textContent = 'Global Variables found:\n' + variables.join('\n');
-    } else {
-      resultContainer.textContent = 'No global variables found.';
-    }
+}
+
+function displayResults(variables) {
+  const resultContainer = document.getElementById('result');
+  if (variables.length > 0) {
+    resultContainer.textContent = 'Global Variables found:\n' + variables.join('\n');
+  } else {
+    resultContainer.textContent = 'No global variables found.';
   }
-  
+}
